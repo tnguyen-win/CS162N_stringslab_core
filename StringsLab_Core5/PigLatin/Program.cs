585f0fb1fd37b5e716a39eabdc6dfe54f237ebe8
tnguyen-win <129979622+tnguyen-win@users.noreply.github.com>
2023-04-11 11:16:04 -0700

    Lab 1 - Draft

--- StringsLab_Core5/PigLatin/Program.cs
+++ StringsLab_Core5/PigLatin/Program.cs
@@ -1,12 +1,123 @@
-﻿using System;
+﻿/*
 
-namespace PigLatin
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+
+    [USEFUL RESOURCE]
+    Pig Latin translator: https://lingojam.com/PigLatinTranslator
+
+
+
+*/
+
+using System;
+using System.Linq;
+
+namespace PigLatin {
+    class Program {
+        static void Main() {
+            Console.WriteLine("Enter a word/phrase/sentence and press ENTER key when done: ");
+
+            string promptString = Console.ReadLine();
+            string[] words = promptString.Split();
+
+            // Problem #1
+            string pig1 = PigLatin1(words[0]);
+            Console.WriteLine($"\n\n\nProblem #1 : {pig1}\n\n\n");
+
+            // Problem #2
+            string pig2 = PigLatin2(words[0]);
+            Console.WriteLine($"Problem #2 : {pig2}\n\n\n");
+
+            // Problem #3
+            string pig3 = PigLatin3(words[0]);
+            Console.WriteLine($"Problem #3 : {pig3}\n\n\n");
+
+            // Problem #4
+            string pig4 = PigLatin4(words);
+            Console.WriteLine($"Problem #4 : {pig4}\n\n");
+        }
+
+        static bool IsVowel(char c) {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            if (vowels.Contains(c)) return true;
+
+            return false;
+        }
+
+        static bool IsPunctuation(string c) {
+            if (char.IsPunctuation(char.Parse(c))) return true;
+
+            return false;
+        }
+
+        static int IndexOfFirstVowel(string s) {
+            int index = -1;
+
+            for (int i = 0; i < s.Length && index == -1; i++) {
+                char c = s[i];
+                if (IsVowel(c)) index = i;
+            }
+
+            return index;
+        }
+
+        static string PigLatin1(string s) {
+            int index = IndexOfFirstVowel(s);
+            string firstPart = s.Substring(index);
+            string secondPart = s.Substring(0, index).ToLower();
+            string thirdPart = "ay";
+
+            return $"{firstPart}{secondPart}{thirdPart}";
+        }
+
+        static string PigLatin2(string s) {
+            int index = IndexOfFirstVowel(s);
+            string firstPart = s.Substring(index);
+
+            if (char.IsUpper(s, 0)) firstPart = $"{s.Substring(index).Substring(0, 1).ToUpper()}{s.Substring(index + 1)}";
+
+            string secondPart = s.Substring(0, index).ToLower();
+            string thirdPart = "ay";
+
+            return $"{firstPart}{secondPart}{thirdPart}";
+        }
+
+        static string PigLatin3(string s) {
+            int index = IndexOfFirstVowel(s);
+            string firstPart = s.Substring(index);
+            string secondPart = s.Substring(0, index).ToLower();
+            string thirdPart = "ay";
+            string fourthPart = s[^1].ToString();
+
+            if (IsPunctuation(s[^1].ToString())) firstPart = firstPart.Remove(firstPart.Length - 1, 1);
+            if (IsPunctuation(fourthPart)) return $"{firstPart}{secondPart}{thirdPart}{fourthPart}";
+
+            return $"{firstPart}{secondPart}{thirdPart}";
+        }
+
+        static string PigLatin4(string[] str) {
+            string allWords = "";
+
+            foreach (string s in str) {
+                int index = IndexOfFirstVowel(s);
+                string firstPart = s.Substring(index);
+
+                if (char.IsUpper(s, 0)) firstPart = $"{s.Substring(index).Substring(0, 1).ToUpper()}{s.Substring(index + 1)}";
+
+                string secondPart = s.Substring(0, index).ToLower();
+                string thirdPart = "ay";
+                string fourthPart = s[^1].ToString();
+
+                if (IsPunctuation(s[^1].ToString())) firstPart = firstPart.Remove(firstPart.Length - 1, 1);
+
+                if (IsPunctuation(fourthPart)) allWords += $"{firstPart}{secondPart}{thirdPart}{fourthPart}";
+                else allWords += $"{firstPart}{secondPart}{thirdPart}";
+
+                if (s != str[^1]) allWords += " ";
+            }
+
+            return allWords;
         }
     }
 }
--- StringsLab_Core5/ShiftCypher/Program.cs
+++ StringsLab_Core5/ShiftCypher/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿/*
 
-namespace ShiftCypher
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+
+
+    [USEFUL RESOURCES]
+    Caesar cypher translator: https://dcode.fr/caesar-cipher
+
+    Long sentence example: https://10fastfingers.com/text/101161-long-sentence
+
+
+
+*/
+
+using System;
+
+namespace ShiftCypher {
+    class Program {
+        public static string englishAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        static void Main() {
+            // Problem #5
+            Console.WriteLine(CaesarCypher("dCode Caesar", 10));
+            Console.WriteLine(CaesarCypher("dCode Caesar", 10));
+            Console.WriteLine(CaesarCypher("dCode Caesar", -10));
+            Console.WriteLine(CaesarCypher("dCode Caesar", -10));
+
+            // Problem #6
+            Console.WriteLine("\n\n\nEnter a word/phrase/sentence containing only English letters (no exceptions) and press ENTER key when done: ");
+
+            string promptString = Console.ReadLine();
+            int rndIndex = new Random().Next(0, 10);
+
+            Console.WriteLine($"\n\nGenerated random shift index: {rndIndex}\n\n\nEncoded Caesar cypher:\n{CaesarCypher(promptString, rndIndex)}\n\n");
+        }
+        static string CaesarCypher(string str, int shift) {
+            string allWords = "";
+            bool capitalized;
+
+            foreach (char s in str) {
+                if (char.IsLetter(s)) {
+                    capitalized = char.IsUpper(s);
+
+                    int remainder = englishAlphabet.IndexOf(s.ToString().ToLower()) + shift;
+
+                    if (capitalized) {
+                        if ((remainder < englishAlphabet.Length) && (remainder >= 0)) allWords += char.Parse(englishAlphabet[remainder].ToString().ToUpper());
+                        else if (remainder >= englishAlphabet.Length) allWords += char.Parse(englishAlphabet[remainder - englishAlphabet.Length].ToString().ToUpper());
+                        else if (remainder <= 0) allWords += char.Parse(englishAlphabet[englishAlphabet.Length + remainder].ToString().ToUpper());
+                    } else {
+                        if (remainder < englishAlphabet.Length && remainder >= 0) allWords += englishAlphabet[remainder];
+                        else if (remainder >= englishAlphabet.Length) allWords += englishAlphabet[remainder - englishAlphabet.Length];
+                        else if (remainder <= 0)  allWords += englishAlphabet[englishAlphabet.Length + remainder];
+                    }
+                } else allWords += s;
+            }
+
+            return allWords;
         }
     }
 }
